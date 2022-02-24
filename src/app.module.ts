569c59e5c07dd7c1ac9e272cfe5450d89cf30d1d
Bruno Bennotti <brunobennotti@gmail.com>
2022-02-24 18:15:28 -0300

    feat: implementação execução de schedule tasks paralelamente a execução da api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ApiModule } from './api/modules/api.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronScheduleTaskModule } from './cron-schedule-tasks/modules/cron-schedule-task.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
+    CronScheduleTaskModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
